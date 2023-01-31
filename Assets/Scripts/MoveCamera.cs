e91094e5be662c2ca200ed66643148dd2f126da2
Akash9933 <38663700+Akash9933@users.noreply.github.com>
2023-01-31 14:46:19 -0800

    Update MoveCamera.cs

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -7,7 +7,7 @@ public class MoveCamera : MonoBehaviour
     public Transform cameraPosition;
 
     // Update is called once per frame
-    void Update()
+   private void Update()
     {
         transform.position = cameraPosition.position;
     }
