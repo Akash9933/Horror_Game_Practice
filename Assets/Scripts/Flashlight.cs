49db677a3fd70c13ded27a0569e32d84c715db69
Akash9933 <38663700+Akash9933@users.noreply.github.com>
2023-02-09 14:57:43 -0800

    Added Flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject light;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            light.SetActive(true);
+        }
+
+        else if(light == true)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                light.SetActive(false);
+            }
+        }
+    }
+
+}
