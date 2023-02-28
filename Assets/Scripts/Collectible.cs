8f9ec9fda3844d22b4ce649ed79560c5bc7a29de
Akash9933 <38663700+Akash9933@users.noreply.github.com>
2023-02-28 11:26:34 -0800

    Add Canvas, and collect Script

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static event Action OnCollected;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localRotation = Quaternion.Euler(180f, Time.time * 100f, 0);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnCollected?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+
 }
